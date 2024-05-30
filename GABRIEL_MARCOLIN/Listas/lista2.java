d18bd130563c8428e5df302a3999bff57ff2d224
Marcoliiin <gabrielmarcolin10@gmail.com>
2024-05-30 10:35:50 -0300

    sextou

--- GABRIEL_MARCOLIN/Listas/lista2.java
+++ GABRIEL_MARCOLIN/Listas/lista2.java
@@ -1,15 +1,15 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class lista2 {
     public static void main(String[] args) {
-        // Atividade 1 Escreva um programa que receba uma lista de números inteiros e
-        // retorne uma lista contendo apenas os números pares usando a Stream API.
-        // A lista deve ter ao menos 8 números.
+        // Atividade 1 
+        //Escreva um programa que receba uma lista de números inteiros e retorne uma lista contendo apenas os números pares usando a Stream API. A lista deve ter ao menos 8 números.
 
-        /* List<Integer> numeros = new ArrayList<>();
+        List<Integer> numeros = new ArrayList<>();
 
         numeros.add(1);
         numeros.add(2);
@@ -23,14 +23,12 @@ public class lista2 {
         List<Integer> numerosPares = numeros.stream().filter(nums -> nums % 2 == 0)
                 .toList();
 
-        numerosPares.forEach(nums -> System.out.println(nums)); */
+        numerosPares.forEach(nums -> System.out.println(nums));
 
         // Atividade 2
-        // Dada a seguinte lista de nomes("roberto", "josé", "caio", "vinicius"),
-        // escreva um programa que converta todos os nomes para letras maiúsculas usando
-        // a Stream API.
+        // Dada a seguinte lista de nomes("roberto", "josé", "caio", "vinicius"),  escreva um programa que converta todos os nomes para letras maiúsculas usando a Stream API.
 
-      /*   List<String> nomes = new ArrayList<>();
+        List<String> nomes = new ArrayList<>();
 
         nomes.add("roberto");
         nomes.add("josé");
@@ -39,16 +37,14 @@ public class lista2 {
 
         List<String> nomesFormatados = nomes.stream().map(nome -> nome.toUpperCase()).toList();
 
-        nomesFormatados.forEach(nome -> System.out.println(nome));*/
+        nomesFormatados.forEach(nome -> System.out.println(nome));
 
-
-        //Atividade 3
-        //Crie um programa que conte quantas vezes cada palavra única aparece em uma lista de strings. 
-        //Utilize a Stream API para processar os dados. 
-        //Lista de palavras("se", "talvez", "hoje" "sábado", "se", "quarta", "sábado")
+        // Atividade 3
+        // Crie um programa que conte quantas vezes cada palavra única aparece em uma lista de strings.
+        // Utilize a Stream API para processar os dados. Lista de palavras("se", "talvez", "hoje" "sábado", "se", "quarta", "sábado").
 
         List<String> palavras = new ArrayList<>();
-        
+
         palavras.add("se");
         palavras.add("talvez");
         palavras.add("hoje");
@@ -57,16 +53,65 @@ public class lista2 {
         palavras.add("quarta");
         palavras.add("sábado");
 
-       // List<String> palavrasUnicas = palavras.stream().distinct().toList();
-       Map<String, Long> contagemPalavras = palavras.stream()
-       .collect(Collectors.groupingBy(word -> word, Collectors.counting()));
+        Map<String, Long> palavrasUnicas = palavras.stream()
+                .collect(Collectors.groupingBy(palavra -> palavra, Collectors.counting()));
 
+        palavrasUnicas.forEach((palavra, quantidade) -> {
+            if (quantidade == 1) {
+                System.out.println(palavra + ": " + quantidade);
+            }
+        });
 
-       contagemPalavras.forEach((palavra, contagem) -> {
-        if (contagem == 1) {
-            System.out.println(palavra + ": " + contagem);
-        }
-    });
+        // Atividade 4
+        // Crie uma Classe Produto, ela possui os atributos nome e preço.  Crie uma lista com 4 objetos do tipo Produto, pode definir os valores
+        // diretamente no código("hard coded"). Escreva um programa que filtre os produtos cujo preço seja maior que R$  100,00 utilizando a Stream API.
 
+        ProdutoLista2 produto1 = new ProdutoLista2();
+        produto1.setNome("Lápis");
+        produto1.setPreco(5);
+
+        ProdutoLista2 produto2 = new ProdutoLista2();
+        produto2.setNome("Carro");
+        produto2.setPreco(100000);
+
+        ProdutoLista2 produto3 = new ProdutoLista2();
+        produto3.setNome("Celular");
+        produto3.setPreco(3000);
+
+        ProdutoLista2 produto4 = new ProdutoLista2();
+        produto4.setNome("Melancia");
+        produto4.setPreco(25);
+
+        List<ProdutoLista2> precosProdutos = new ArrayList<>();
+
+        precosProdutos.add(produto1);
+        precosProdutos.add(produto2);
+        precosProdutos.add(produto3);
+        precosProdutos.add(produto4);
+
+        List<ProdutoLista2> produtosCaros = precosProdutos.stream().filter(precos -> precos.getPreco() > 100).toList();
+
+        produtosCaros
+                .forEach(precos -> System.out.println("Produto: " + precos.getNome() + " Valor: " + precos.getPreco()));
+
+        // Atividade 5
+        // Realize a soma do valor total dos Produtos que estão na lista de produtos  criados na atv4, realize essa soma utilizando Stream API.
+
+        // Levei em consideração todos os produtos, e não somente os que tinham o preço  maior que 100
+        Double somaProdutos = precosProdutos.stream().map(precos -> precos.getPreco()).reduce(0d,
+                (valorAnterior, valorAtual) -> valorAnterior + valorAtual);
+
+        System.out.println("Soma total dos produtos da lista 4: " + somaProdutos);
+
+        // Atividade 6
+        // Dada a lista("Java", "Python", "C", "JavaScript", "Ruby"), ordene a mesma conforme o tamanho da palavra, da menor para a maior, utilizando a Stream API.
+
+        List<String> linguagens = Arrays.asList("Java", "Python", "C", "JavaScript", "Ruby");
+
+        List<String> linguagensOrdenadas = linguagens.stream()
+                .sorted((s1, s2) -> Integer.compare(s1.length(), s2.length()))
+                .toList();
+
+        linguagensOrdenadas.forEach(ordem -> System.out.println(ordem));
     }
 }
---
+++ GABRIEL_MARCOLIN/Listas/lista3.java
@@ -0,0 +1,135 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JOptionPane;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class lista3 {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Lista 3 do Professor Sandro");
+
+        frame.setSize(300, 300);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JPanel panel = new JPanel(new BorderLayout());
+        JPanel painelDeBotoes = new JPanel();
+        painelDeBotoes.setLayout(new BoxLayout(painelDeBotoes, BoxLayout.Y_AXIS));
+
+        JLabel label = new JLabel("Escolha uma opção");
+        label.setHorizontalAlignment(JLabel.CENTER);
+
+        JButton botaoHello = new JButton("Exibir mensagem");
+        JButton botaoNome = new JButton("Digitar nome");
+        JButton botaoCalculadora = new JButton("Calculadora");
+        JButton botaoContinuar = new JButton("Deseja continuar?");
+
+        botaoHello.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                JOptionPane.showMessageDialog(frame, "Olá, Mundo!");
+            }
+        });
+
+        botaoNome.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                String nome = JOptionPane.showInputDialog(frame, "Digite seu nome:");
+                if (nome != null) {
+                    JOptionPane.showMessageDialog(frame, "Olá, " + nome + "!");
+                } else {
+                    JOptionPane.showMessageDialog(frame, "Nenhum nome inserido.", "Erro", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        botaoContinuar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                int resposta = JOptionPane.showConfirmDialog(frame, "Você deseja continuar?", "Confirmação",
+                        JOptionPane.YES_NO_OPTION);
+                if (resposta == JOptionPane.YES_OPTION) {
+                    JOptionPane.showMessageDialog(frame, "Continuando!");
+                } else {
+                    JOptionPane.showMessageDialog(frame, "Encerrando programa...");
+                    frame.dispose(); // Fecha a janela
+                }
+            }
+        });
+
+        botaoCalculadora.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent event) {
+                String[] options = { "Soma", "Subtração", "Multiplicação", "Divisão" };
+                int escolha = JOptionPane.showOptionDialog(frame, "Escolha uma operação", "Calculadora",
+                        JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE, null, options, options[0]);
+
+                if (escolha == JOptionPane.CLOSED_OPTION)
+                    return;
+
+                String resposta1 = JOptionPane.showInputDialog(frame, "Digite o primeiro número:");
+                String resposta2 = JOptionPane.showInputDialog(frame, "Digite o segundo número:");
+
+                if (resposta1 == null || resposta2 == null) {
+                    JOptionPane.showMessageDialog(frame, "Erro, por favor digite dois números.", "Erro",
+                            JOptionPane.ERROR_MESSAGE);
+                    return;
+                }
+
+                double num1 = Double.parseDouble(resposta1);
+                double num2 = Double.parseDouble(resposta2);
+                double resultado = 0;
+
+                switch (escolha) {
+                    case 0:
+                        resultado = num1 + num2;
+                        break;
+                    case 1:
+                        resultado = num1 - num2;
+                        break;
+                    case 2:
+                        resultado = num1 * num2;
+                        break;
+                    case 3:
+                        if (num2 == 0) {
+                            JOptionPane.showMessageDialog(frame, "Não existe divisão por zero!!!", "Erro",
+                                    JOptionPane.ERROR_MESSAGE);
+
+                        } else {
+                            resultado = num1 / num2;
+                        }
+                        break;
+                }
+                JOptionPane.showMessageDialog(frame, "O resultado é: " + resultado, "Resultado",
+                        JOptionPane.INFORMATION_MESSAGE);
+
+            }
+        });
+
+        botaoHello.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        botaoNome.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        botaoContinuar.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        botaoCalculadora.setAlignmentX(JButton.CENTER_ALIGNMENT);
+
+        painelDeBotoes.add(Box.createRigidArea(new Dimension(10, 50)));
+        painelDeBotoes.add(botaoHello);
+        painelDeBotoes.add(Box.createRigidArea(new Dimension(10, 10)));
+        painelDeBotoes.add(botaoNome);
+        painelDeBotoes.add(Box.createRigidArea(new Dimension(10, 10)));
+        painelDeBotoes.add(botaoCalculadora);
+        painelDeBotoes.add(Box.createRigidArea(new Dimension(10, 10)));
+        painelDeBotoes.add(botaoContinuar);
+
+        panel.add(label, BorderLayout.NORTH);
+        panel.add(painelDeBotoes, BorderLayout.CENTER);
+
+        frame.add(panel);
+        frame.setVisible(true);
+    }
+}
