c293660a48d2ef51e1393fa5bb04506ebfa57393
Marcelo Casa <mppozzobon8@gmail.com>
2024-06-06 10:53:01 -0300

    commit teste

--- MARCELO_PILUSKI_POZZOBON/SegundoBI/listas/lista2/Main.java
+++ MARCELO_PILUSKI_POZZOBON/SegundoBI/listas/lista2/Main.java
@@ -3,6 +3,7 @@ package SegundoBI.listas.lista2;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
---
+++ MARCELO_PILUSKI_POZZOBON/SegundoBI/listas/lista3/Main.java
@@ -0,0 +1,13 @@
+package SegundoBI.listas.lista3;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Hello World");
+        frame.setSize(500,500);
+        frame.setVisible(true);
+    }
+}
