bc68849cc9608ab5ca24e2f44c105050f37255ee
AndersonTessaro <astessaro@minha.fag.edu.br>
2024-06-27 21:38:37 -0300

    lista4

---
+++ ANDERSON_TESSARO/testes/JsonUtils.java
@@ -0,0 +1,12 @@
+package testes;
+
+import java.util.Map;
+
+public class JsonUtils {
+
+    public static Map<String, Object> srtToMap(Object token) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'srtToMap'");
+    }
+
+}
