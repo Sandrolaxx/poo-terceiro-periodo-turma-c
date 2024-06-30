6f4704c9ea63d18a5203c987bc2faa8b7c5c5054
Rafael Roani Gonçalves <rafaelgoncalvesroani@gmail.com>
2024-06-29 23:53:15 -0300

    Correção das Classes que eu deletei não sei como

---
+++ RAFAEL_ROANI/src/main/java/primeirobimestre/list7/service/LojaService.java
@@ -0,0 +1,23 @@
+package primeirobimestre.list7.service;
+
+import primeirobimestre.list7.domain.domains.Loja;
+import primeirobimestre.list7.repository.LojaRepository;
+
+public class LojaService {
+    private LojaRepository lojaRepository = new LojaRepository();
+    private static long id = 1;
+
+    public void cadastrarLoja(Loja loja) {
+        if (loja == null) throw new NullPointerException();
+        loja.setId(id++);
+        lojaRepository.cadastrarLoja(loja);
+    }
+
+    public void printListLojas() {
+        lojaRepository.printListLojas();
+    }
+
+    public Loja findByIdOrThrowIllegalArgumentException(Long id) {
+        return lojaRepository.findByIdOrThrowIllegalArgumentException(id);
+    }
+}
